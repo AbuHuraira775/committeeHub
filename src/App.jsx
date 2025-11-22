36738e2e6cd8825b8dd5af3e36ebd9749e2ad56f
Abu Huraira <abuhurairah775@gmail.com>
2025-11-23 02:05:20 +0500

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import './index.css'
 import allRoutes from './routes/allRoutes'
-import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-   <>
-   <Routes>
-    {allRoutes.map((route,index)=>{
-      return <Route key={index} path={route.path} element={route.component} />
-    })}
-   </Routes>
-   </>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/members" />} />
+        {allRoutes.map((route, index) => {
+          return <Route key={index} path={route.path} element={route.component} />
+        })}
+      </Routes>
+    </>
   )
 }
 
